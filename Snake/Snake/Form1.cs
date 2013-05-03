5505555cf94afad14cd5e35d9574624d1a06260c
astrutynskyy <andriy.strutynskyy@gmail.com>
2013-05-03 18:18:34 +0300

    Clean up project (remove Form1.resx, Grid.cs)

--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -11,13 +11,9 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
-        private Grid grid = null;
-
         public Form1()
         {
             InitializeComponent();
-            grid = new Grid(500,500);
-            Controls.Add(grid);
         }
 
         private void Form1_Load(object sender, EventArgs e)
